0e8205217847c30231e0290f600852b4ff09fe9a
WatxoGitHub <watxo0211@gmail.com>
2020-09-15 14:01:38 -0500

    sms echo script en archivo php

--- enviar.php
+++ enviar.php
@@ -5,16 +5,16 @@ $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
 $mensaje = $_POST["mensaje"];
-echo ("mesaje 1");
+echo "<script> alert ('mesaje 1') </script>";
 // Datos para el correo
 $destinatario = "dekontruccion@gmail.com";
 $asunto = "Contacto desde nuestra web";
-echo ("mesaje 2");
+echo "<script> alert ('mesaje dos') </script>";
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
 $carta .= "Telefono: $telefono \n";
 $carta .= "Mensaje: $mensaje";
-echo ("mesaje 3");
+echo "<script> alert ('mesaje tres') </script>";
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta);
 header("Location:mensaje-de-envio.html");
